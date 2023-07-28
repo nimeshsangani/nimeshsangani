ac89b5058a6a6196eb659b84a769127d2e5d2641
nimeshsangani <140734795+nimeshsangani@users.noreply.github.com>
2023-07-28 11:33:13 +0530

    Add files via upload

---
+++ square.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+
+    printf("Number\tSquare\tCube\n");
+    printf("your ans\n");
+
+    for(i=0; i<=50; i++)
+        printf("%d\t%d\t%d\n", i, i*i, i*i*i);
+    
+    return 0;
+}
