e4f4495ca323beac84e6dd525eb27495374bf0a2
nimeshsangani <140734795+nimeshsangani@users.noreply.github.com>
2023-07-28 11:30:20 +0530

    Add files via upload

---
+++ introduction.c.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main ()
+{
+    printf("Name:-sangani nimesh\n");
+    printf("birthdate:-05|11|2005\n");
+    printf("age:-17\n");
+    printf("address:-bapasitaram chowk,angal-park,mavdi,rajkot");
+
+    return 0;
+}
