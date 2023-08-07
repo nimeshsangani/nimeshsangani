5ac09678549242938aa23f8c9a955ba4a96b5e18
nimeshsangani <140734795+nimeshsangani@users.noreply.github.com>
2023-08-07 19:31:46 +0530

    Add files via upload

---
+++ simpleinterest.c
@@ -0,0 +1,15 @@
+// mark and result-
+#include<stdio.h>
+int main()
+{
+   int p,r,n;
+   float i;
+   printf("4 sub mark:-\n");
+   scanf("%d\t %d\t %d\t",&p,&r,&n);
+
+   i=p*r*n/100;
+
+   printf("simple interest is : %.2f",i);
+   return 0;
+
+}
--- square.c
+++ square.c
@@ -1,13 +1,14 @@
+// number,square and cube 
 #include<stdio.h>
 int main()
 {
     int i;
 
     printf("Number\tSquare\tCube\n");
-    printf("your ans\n");
+    printf("your ans is=:\n");
 
     for(i=0; i<=50; i++)
-        printf("%d\t%d\t%d\n", i, i*i, i*i*i);
+        printf("%d\t%d\t%d\n", i, i*i, i*i*i); 
     
     return 0;
 }
